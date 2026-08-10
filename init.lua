cf120c61495d568020d72e2509ab952c8f65de88
Harmans <harmans6282@gmail.com>
2026-08-10 17:15:45 +0530

    nvtree show .gitignored files

--- init.lua
+++ init.lua
@@ -29,6 +29,14 @@ require("lazy").setup({
       },
     },
   },
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = {
+      filters = {
+        git_ignored = false, -- show gitignored files/folders too
+      },
+    },
+  },
 
   { import = "plugins" },
 }, lazy_config)
