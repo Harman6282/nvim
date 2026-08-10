c2fe3f097f9248cae8d6d02ddfe923f5b417b206
Harmans <harmans6282@gmail.com>
2026-08-10 17:10:27 +0530

    v terminal resize

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- custom vertical terminal with bigger width
+map("n", "<leader>v", function()
+  require("nvchad.term").toggle({ pos = "vsp", size = 0.4, id = "vtoggleTerm" })
+end, { desc = "terminal toggle vertical term" })
+
+map("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
