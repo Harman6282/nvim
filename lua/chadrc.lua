33512c56e4c58a9ef358998d760637286c59558a
Harmans <harmans6282@gmail.com>
2026-08-13 01:47:18 +0530

    transparency

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -6,7 +6,8 @@
 local M = {}
 
 M.base46 = {
-  theme = "tokyonight",
+  theme = "kanagawa",
+  transparency = true,
   hl_override = {
     Comment = { italic = true },
     ["@comment"] = { italic = true },
